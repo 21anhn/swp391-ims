22241240f003383ee83f0672f631d92a753015b6
Nhat Anh <ltnanh21@gmail.com>
2024-05-23 08:57:29 +0700

    Create IMS project

---
+++ backend/ims_application/src/main/java/com/swp391/ims_application/ImsApplication.java
@@ -0,0 +1,13 @@
+package com.swp391.ims_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImsApplication.class, args);
+	}
+
+}
---
+++ backend/ims_application/src/test/java/com/swp391/ims_application/ImsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp391.ims_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
