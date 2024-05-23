83a0d2218c43b757b1f1b397e0b44b21d00dbc74
Nhat Anh <ltnanh21@gmail.com>
2024-05-23 10:01:11 +0700

    Create InternshipCampaign.java

---
+++ backend/ims_application/src/main/java/com/swp391/ims_application/entity/InternshipCampaign.java
@@ -0,0 +1,90 @@
+package com.swp391.ims_application.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Table(name = "internship_campaigns")
+@Entity
+public class InternshipCampaign {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "campaign_id")
+    private int campaignId;
+
+    @Column(name = "campaign_name")
+    private String campaignName;
+
+    @Column(name = "job_description")
+    private String jobDescription;
+
+    @Column(name = "requirements")
+    private String requirements;
+
+    @Column(name = "posted_date")
+    private Date postedDate;
+
+    @Column(name = "deadline")
+    private Date deadline;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private User userHR;
+
+    public int getCampaignId() {
+        return campaignId;
+    }
+
+    public void setCampaignId(int campaignId) {
+        this.campaignId = campaignId;
+    }
+
+    public String getCampaignName() {
+        return campaignName;
+    }
+
+    public void setCampaignName(String campaignName) {
+        this.campaignName = campaignName;
+    }
+
+    public String getJobDescription() {
+        return jobDescription;
+    }
+
+    public void setJobDescription(String jobDescription) {
+        this.jobDescription = jobDescription;
+    }
+
+    public String getRequirements() {
+        return requirements;
+    }
+
+    public void setRequirements(String requirements) {
+        this.requirements = requirements;
+    }
+
+    public Date getPostedDate() {
+        return postedDate;
+    }
+
+    public void setPostedDate(Date postedDate) {
+        this.postedDate = postedDate;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public User getUserHR() {
+        return userHR;
+    }
+
+    public void setUserHR(User userHR) {
+        this.userHR = userHR;
+    }
+}
