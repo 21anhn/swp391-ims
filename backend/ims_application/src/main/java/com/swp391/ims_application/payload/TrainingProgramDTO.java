7810096f61081ddc75942cd2151a24bae1a1183d
Nhat Anh <ltnanh21@gmail.com>
2024-06-21 21:25:59 +0700

    API: Manage training program

---
+++ backend/ims_application/src/main/java/com/swp391/ims_application/payload/TrainingProgramDTO.java
@@ -0,0 +1,13 @@
+package com.swp391.ims_application.payload;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TrainingProgramDTO {
+    private int programId;
+    private String programName;
+    private String description;
+    private String objectives;
+}
