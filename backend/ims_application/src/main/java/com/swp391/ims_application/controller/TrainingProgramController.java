1f29630218637a68d76ea524e61528af566641f4
Vo Le Duc Anh <voleducanh93@gmail.com>
2024-06-23 23:14:53 +0700

    Specify intern to training program (Actor: Internship Coordinator) 1. Add intern account to training program 2. Remove intern account in training program 3. View all intern account --> format line

--- backend/ims_application/src/main/java/com/swp391/ims_application/controller/TrainingProgramController.java
+++ backend/ims_application/src/main/java/com/swp391/ims_application/controller/TrainingProgramController.java
@@ -82,7 +82,7 @@ public class TrainingProgramController {
 
 
     @GetMapping("/{programId}/interns")
-    public ResponseEntity<?> getAllInternsInTrainingProgram(@PathVariable int programId) {
+    public ResponseEntity<?> getAllInternsInTrainingProgram(@PathVariable int programId) { //format line
         List<AccountDTO> internDTOs = trainingProgramService.getAllInternsInTrainingProgram(programId);
         if (!internDTOs.isEmpty()) {
             return new ResponseEntity<>(internDTOs, HttpStatus.OK);
