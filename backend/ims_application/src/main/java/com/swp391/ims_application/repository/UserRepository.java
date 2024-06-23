4ec2901263e23a85d63e8272aeb8fe5e68b67519
Vo Le Duc Anh <voleducanh93@gmail.com>
2024-06-23 14:59:28 +0700

    Số lượng account theo từng role Số lượng account trong system

--- backend/ims_application/src/main/java/com/swp391/ims_application/repository/UserRepository.java
+++ backend/ims_application/src/main/java/com/swp391/ims_application/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.swp391.ims_application.repository;
 
 import com.swp391.ims_application.entity.User;
-import com.swp391.ims_application.payload.RoleAccountCountDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -19,10 +18,5 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     List<User> findByRoleRoleName(String roleName);
 
-    @Query("SELECT new com.swp391.ims_application.payload.RoleAccountCountDTO(u.role.roleName, COUNT(u)) " +
-            "FROM User u GROUP BY u.role.roleName")
-    List<RoleAccountCountDTO> countAccountsByRole();
-
-    @Query("SELECT COUNT(u) FROM User u")
-    long countTotalUsers();
+    long count();
 }
