8bc67e5d33f7c3fdb0abf466fc5c750639d6fd81
Vo Le Duc Anh <voleducanh93@gmail.com>
2024-06-23 22:14:00 +0700

    Specify intern to training program (Actor: Internship Coordinator) 1. Add intern account to training program 2. Remove intern account in training program 3. View all intern account

--- backend/ims_application/src/main/java/com/swp391/ims_application/service/TrainingProgramService.java
+++ backend/ims_application/src/main/java/com/swp391/ims_application/service/TrainingProgramService.java
@@ -1,20 +1,33 @@
 package com.swp391.ims_application.service;
 
 import com.swp391.ims_application.entity.TrainingProgram;
+import com.swp391.ims_application.entity.TrainingProgramIntern;
+import com.swp391.ims_application.entity.User;
+import com.swp391.ims_application.payload.AccountDTO;
 import com.swp391.ims_application.payload.TrainingProgramDTO;
+import com.swp391.ims_application.repository.TrainingProgramInternRepository;
 import com.swp391.ims_application.repository.TrainingProgramRepository;
+import com.swp391.ims_application.repository.UserRepository;
 import com.swp391.ims_application.service.imp.ITrainingProgramService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TrainingProgramService implements ITrainingProgramService {
 
     @Autowired
     private TrainingProgramRepository trainingProgramRepository;
+
+    @Autowired
+    private UserRepository userRepository; // Để lấy thông tin User
+
+    @Autowired
+    private TrainingProgramInternRepository trainingProgramInternRepository;
 
     @Override
     public boolean createTrainingProgram(TrainingProgramDTO trainingProgramDTO) {
@@ -79,4 +92,82 @@ public class TrainingProgramService implements ITrainingProgramService {
         trainingProgramRepository.save(trainingProgram);
         return true;
     }
+
+    @Override
+    public boolean addInternToTrainingProgram(int programId, int internId) {
+        Optional<TrainingProgram> optionalProgram = trainingProgramRepository.findById(programId);
+        Optional<User> optionalIntern = userRepository.findById(internId);
+
+        if (optionalProgram.isPresent() && optionalIntern.isPresent()) {
+            TrainingProgram program = optionalProgram.get();
+            User intern = optionalIntern.get();
+
+            List<TrainingProgramIntern> interns = program.getTrainingProgramInterns();
+            for (TrainingProgramIntern tpi : interns) {
+                if (tpi.getUserIntern().getUserId() == internId) {
+                    return false;
+                }
+            }
+
+            TrainingProgramIntern newInternship = new TrainingProgramIntern();
+            newInternship.setTrainingProgram(program);
+            newInternship.setUserIntern(intern);
+            trainingProgramInternRepository.save(newInternship);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean removeInternFromTrainingProgram(int programId, int internId) {
+        Optional<TrainingProgram> optionalProgram = trainingProgramRepository.findById(programId);
+        Optional<User> optionalIntern = userRepository.findById(internId);
+
+        if (optionalProgram.isPresent() && optionalIntern.isPresent()) {
+            TrainingProgram program = optionalProgram.get();
+            User intern = optionalIntern.get();
+
+            List<TrainingProgramIntern> interns = program.getTrainingProgramInterns();
+            for (TrainingProgramIntern tpi : new ArrayList<>(interns)) {
+                if (tpi.getUserIntern().getUserId() == internId) {
+                    trainingProgramInternRepository.delete(tpi);
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public List<AccountDTO> getAllInternsInTrainingProgram(int programId) {
+        Optional<TrainingProgram> optionalProgram = trainingProgramRepository.findById(programId);
+
+        if (optionalProgram.isPresent()) {
+            TrainingProgram program = optionalProgram.get();
+            List<AccountDTO> internDTOs = new ArrayList<>();
+
+            List<TrainingProgramIntern> interns = program.getTrainingProgramInterns();
+            for (TrainingProgramIntern tpi : interns) {
+                User intern = tpi.getUserIntern();
+                AccountDTO accountDTO = new AccountDTO();
+                accountDTO.setId(intern.getUserId());
+                accountDTO.setUsername(intern.getUsername());
+                accountDTO.setEmail(intern.getEmail());
+                accountDTO.setPhoneNumber(intern.getPhoneNumber());
+                accountDTO.setRoleName(intern.getRole().getRoleName());
+
+                internDTOs.add(accountDTO);
+            }
+
+            return internDTOs;
+        }
+
+        return Collections.emptyList();
+    }
+
+
+
 }
