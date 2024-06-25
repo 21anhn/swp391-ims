328133f3a234338b927f4ac53d17e0b178337326
Đức Hậu <138268743+phanduchau@users.noreply.github.com>
2024-06-25 13:53:38 +0700

    View feedback(intern task) (#58)

--- backend/ims_application/src/main/java/com/swp391/ims_application/service/imp/IInternTaskService.java
+++ backend/ims_application/src/main/java/com/swp391/ims_application/service/imp/IInternTaskService.java
@@ -1,10 +1,11 @@
 package com.swp391.ims_application.service.imp;
 
 import com.swp391.ims_application.entity.InternTask;
+import com.swp391.ims_application.payload.FeedbackDTO;
 
 public interface IInternTaskService {
 
     InternTask saveInternTask(InternTask internTask);
     InternTask findInternTaskById(int id);
-    String getMentorFeedbackByTaskId(int taskId);
+    FeedbackDTO getMentorFeedbackByTaskId(int taskId);
 }
